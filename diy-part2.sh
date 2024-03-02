471c820c9c313e9e80c669adb62dd7fc54647385
junjun1121 <136947888+junjun1121@users.noreply.github.com>
2024-03-02 12:39:57 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -17,9 +17,11 @@ rm -rf feeds/luci/applications/luci-app-passwall
 rm -rf feeds/luci/applications/luci-app-alist
 rm -rf feeds/luci/themes/luci-theme-argon
 rm -rf feeds/packages/net/mosdns
+rm -rf feeds/packages/net/alist
+rm -rf feeds/packages/lang/golang
 sed -i 's/192.168.1.1/10.10.10.1/g' package/base-files/files/bin/config_generate
+git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/lang/golang
 git clone https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
 git clone https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
 git clone https://github.com/sbwml/luci-app-alist package/luci-app-alist
 git clone -b main https://github.com/xiaorouji/openwrt-passwall package/psw
-git clone https://github.com/sbwml/luci-app-mosdns package/mosdns
